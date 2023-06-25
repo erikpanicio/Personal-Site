cde4547a94f86073b2789d4a7053e72c0e16b444
Erik Panicio <erikpanicio.dev@gmail.com>
2023-06-25 03:31:03 -0300

    antes do menu

---
+++ Portfolio/script.js
@@ -0,0 +1,12 @@
+/* rolagem suave do overflow-image */
+
+/* function overflowMovimentDown() {
+	let overflow_projects = document.querySelector(".overflow-image")
+	overflow_projects.scrollTo(0, overflow_projects.scrollHeight)
+}
+
+function overflowMovimentUp() {
+	let overflow_projects = document.querySelector(".overflow-image")
+	overflow_projects.scrollTo(0, 0)
+}
+ */
